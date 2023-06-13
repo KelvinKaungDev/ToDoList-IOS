396df9764179b2a37e1dad9937a7b9a6f9e3d300
Kelvin Gao <u6511753@au.edu>
2023-06-13 15:38:53 +0700

    Finished To Do list by my own

--- ToDoList/ListViewController.swift
+++ ToDoList/ListViewController.swift
@@ -5,9 +5,20 @@ class ListViewController: UITableViewController {
     
     var lists = ["Eating", "Sleeping"]
 
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let userLists = defaults.array(forKey: "Lists") {
+            lists = userLists as! [String]
+        }
     }
+}
+
+//For the Table View and Cell
+
+extension ListViewController {
     
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
@@ -33,12 +44,19 @@ class ListViewController: UITableViewController {
         
         tableView.deselectRow(at: indexPath, animated: true)
    }
+}
+
+//For the Local Storage for User List
+
+extension ListViewController {
     
     @IBAction func addList(_ sender: UIBarButtonItem) {
         var newLists = UITextField()
         let alert =  UIAlertController(title: "Add To Do List", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add List", style: .default) { (action) in
             self.lists.append(newLists.text!)
+            self.defaults.set(self.lists, forKey: "Lists")
+            
             self.tableView.reloadData()
         }
         
